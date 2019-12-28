971709dad810498c6665e57179d403d10f3a95ae
SumanDas3001 <sumandas3001@gmail.com>
2019-12-29 01:11:12 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import Person from './Person/Person';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey, I'm React App</h1>
+      <p>This is really working.</p>
+      <Person name='suman' age='23' />
+      <Person name='rahul' age='28'/>
+      <Person name='amit' age='33'/>
     </div>
   );
+    // return React.createElement('div', null, React.createElement('h1', null, 'Hi, I\m React App!!!'));
 }
 
 export default App;
